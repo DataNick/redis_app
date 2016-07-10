ed5aec8c834a0debb4298b9dbfcb1001494a5909
DataNick <DataNick@users.noreply.github.com>
2016-07-10 18:14:56 -0400

    Added patch routes for upvote and downvote

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   root 'letters#index'
-  resources :letters
+  resources :letters, only: [:index] do
+    member do
+      patch :upvote # /letters/:id/upvote
+      patch :downvote # /letters/:id/downvote
+    end
+  end
+
 end
