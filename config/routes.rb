2c5e958476e8c0e519c28057f49b332d1c97dd51
DataNick <DataNick@users.noreply.github.com>
2016-07-11 01:40:18 -0400

    Modified standard and delayed routes and emails index route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'emails' => "emails#index"
+
+  post 'emails/standard'
+
+  post 'emails/delayed'
+
   root 'letters#index'
   resources :letters, only: [:index] do
     member do
